d50b19acd09dfc43757bdfa18986630fd2afe6db
Andrew Champion <andrew.champion@gmail.com>
2018-08-28 13:09:24 -0400

    Gunpowder initial working 2D unet

---
+++ gunpowder_ttc.py
@@ -0,0 +1,168 @@
+from __future__ import print_function
+import gunpowder as gp
+import json
+import math
+
+def train(iterations):
+
+    ##################
+    # DECLARE ARRAYS #
+    ##################
+
+    # raw intensities
+    raw = gp.ArrayKey('RAW')
+
+    # objects labelled with unique IDs
+    gt_labels = gp.ArrayKey('LABELS')
+
+    # weights to use to balance the loss
+    loss_weights = gp.ArrayKey('LOSS_WEIGHTS')
+
+    # the predicted labels
+    pred_labels = gp.ArrayKey('PRED_LABELS')
+
+    # the gredient of the loss wrt to the predicted labels
+    pred_labels_gradients = gp.ArrayKey('PRED_LABELS_GRADIENTS')
+
+    ####################
+    # DECLARE REQUESTS #
+    ####################
+
+    with open('train_net_config.json', 'r') as f:
+        net_config = json.load(f)
+
+    # get the input and output size in world units (nm, in this case)
+    voxel_size = gp.Coordinate((48, 48))
+    input_size = gp.Coordinate(net_config['input_shape'])*voxel_size
+    output_size = gp.Coordinate(net_config['output_shape'])*voxel_size
+
+    # formulate the request for what a batch should (at least) contain
+    request = gp.BatchRequest()
+    request.add(raw, input_size)
+    request.add(gt_labels, output_size)
+    # request.add(loss_weights, output_size)
+
+    # when we make a snapshot for inspection (see below), we also want to
+    # request the predicted affinities and gradients of the loss wrt the
+    # affinities
+    snapshot_request = gp.BatchRequest()
+    snapshot_request[pred_labels] = request[gt_labels]
+    snapshot_request[pred_labels_gradients] = request[gt_labels]
+
+    ##############################
+    # ASSEMBLE TRAINING PIPELINE #
+    ##############################
+
+    pipeline = (
+
+        gp.DirectorySource(
+            '/home/championa/data/nadine/1018/sequence_export',
+            {
+                raw: 'z=0.0 to z=1170000-1.tif',
+                gt_labels: 'Labels3.tif'
+            },
+            {
+                raw: gp.ArraySpec(voxel_size=(48,48), interpolatable=True),
+                gt_labels: gp.ArraySpec(voxel_size=(48,48), interpolatable=False),
+            }
+        ) +
+
+        # convert raw to float in [0, 1]
+        gp.Normalize(raw) +
+
+        # chose a random location for each requested batch
+            gp.RandomLocation() +
+
+        # apply transpose and mirror augmentations
+        gp.SimpleAugment() +
+
+        # scale and shift the intensity of the raw array
+        gp.IntensityAugment(
+            raw,
+            scale_min=0.9,
+            scale_max=1.1,
+            shift_min=-0.1,
+            shift_max=0.1) +
+
+        gp.ExcludeLabels(
+            gt_labels,
+            [0],
+            background_value=1) +
+
+        gp.IntensityScaleShift(
+            gt_labels,
+            scale=1,
+            shift=-1) +
+
+        gp.PreCache(cache_size=400, num_workers=4) +
+
+        # create a weight array that balances positive and negative samples in
+        # the affinity array
+        # gp.BalanceLabels(
+        #     gt_labels,
+        #     loss_weights) +
+
+        # pre-cache batches from the point upstream
+        # gp.PreCache(
+        #     cache_size=10,
+        #     num_workers=5) +
+
+        # perform one training iteration for each passing batch (here we use
+        # the tensor names earlier stored in train_net.config)
+        gp.tensorflow.Train(
+            'train_net',
+            net_config['optimizer'],
+            net_config['loss'],
+            inputs={
+                net_config['raw']: raw,
+                net_config['gt_labels']: gt_labels,
+                # net_config['loss_weights']: loss_weights
+            },
+            outputs={
+                net_config['pred_labels']: pred_labels
+            },
+            gradients={
+                net_config['pred_labels_swap']: pred_labels_gradients
+            },
+            save_every=1000000,
+            log_dir='log',
+            log_every=100,
+            summary=net_config['summary'],
+            # array_specs={
+            #     pred_labels: gp.ArraySpec(voxel_size=(48,48,1)),
+            #     pred_labels_gradients: gp.ArraySpec(voxel_size=())
+            # }
+            ) +
+
+        # save the passing batch as an HDF5 file for inspection
+        # gp.Snapshot(
+        #     {
+        #         raw: '/volumes/raw',
+        #         gt_labels: '/volumes/labels',
+        #         pred_labels: '/volumes/pred_labels',
+        #         pred_labels_gradients: '/volumes/pred_labels_gradients'
+        #     },
+        #     output_dir='snapshots',
+        #     output_filename='batch_{iteration}.hdf',
+        #     every=100,
+        #     additional_request=snapshot_request,
+        #     compression_type='gzip') +
+
+        # show a summary of time spend in each node every 10 iterations
+        gp.PrintProfilingStats(every=10)
+    )
+
+    #########
+    # TRAIN #
+    #########
+
+    print("Training for", iterations, "iterations")
+
+    with gp.build(pipeline):
+        for i in range(iterations):
+            pipeline.request_batch(request)
+
+    print("Finished")
+
+if __name__ == "__main__":
+    train(200000)
---
+++ mknet.py
@@ -0,0 +1,89 @@
+from gunpowder.zoo.tensorflow import unet, conv_pass
+import tensorflow as tf
+import json
+
+def create_network(input_shape, name):
+
+    tf.reset_default_graph()
+
+    # create a placeholder for the 3D raw input tensor
+    raw = tf.placeholder(tf.float32, shape=input_shape)
+
+    # create a U-Net
+    raw_batched = tf.reshape(raw, (1, 1) + input_shape)
+    unet_output = unet(raw_batched, 4, 4, [[3,3],[3,3],[3,3]])
+
+    # add a convolution layer to create 3 output maps representing affinities
+    # in z, y, and x
+    pred_affs_batched = conv_pass(
+        unet_output,
+        kernel_size=1,
+        num_fmaps=4,
+        num_repetitions=1,
+        activation=None)
+
+    # get the shape of the output
+    output_shape_batched = pred_affs_batched.get_shape().as_list()
+    output_shape = output_shape_batched[2:] # strip the batch dimension
+    output_shape = [56, 56]
+
+    # the 4D output tensor (3, depth, height, width)
+    pred_affs_swap = tf.reshape(pred_affs_batched, output_shape_batched[1:])
+    pred_affs_swap = tf.transpose(pred_affs_swap, [1, 2, 0])
+
+    # create a placeholder for the corresponding ground-truth affinities
+    gt_affs = tf.placeholder(tf.int32, shape=output_shape)
+
+    # create a placeholder for per-voxel loss weights
+    # loss_weights = tf.placeholder(
+    #     tf.float32,
+    #     shape=output_shape)
+    print(gt_affs.get_shape().as_list())
+    print(pred_affs_swap.get_shape().as_list())
+    # print(loss_weights.get_shape().as_list())
+
+    # compute the loss as the weighted mean squared error between the
+    # predicted and the ground-truth affinities
+    loss = tf.losses.sparse_softmax_cross_entropy(
+        gt_affs,
+        pred_affs_swap)
+
+    # use the Adam optimizer to minimize the loss
+    opt = tf.train.AdamOptimizer(
+        learning_rate=0.5e-4,
+        beta1=0.95,
+        beta2=0.999,
+        epsilon=1e-8)
+    optimizer = opt.minimize(loss)
+
+    tf.summary.scalar("loss", loss)
+    merged_summary_op = tf.summary.merge_all()
+
+    pred_affs = tf.transpose(pred_affs_swap, [2, 0, 1])
+
+    # store the network in a meta-graph file
+    tf.train.export_meta_graph(filename=name + '.meta')
+
+    # store network configuration for use in train and predict scripts
+    config = {
+        'raw': raw.name,
+        'pred_labels_swap': pred_affs_swap.name,
+        'pred_labels': pred_affs.name,
+        'gt_labels': gt_affs.name,
+        # 'loss_weights': loss_weights.name,
+        'loss': loss.name,
+        'optimizer': optimizer.name,
+        'input_shape': input_shape,
+        'output_shape': output_shape,
+        'summary': merged_summary_op.name
+    }
+    with open(name + '_config.json', 'w') as f:
+        json.dump(config, f)
+
+if __name__ == "__main__":
+
+    # create a network for training
+    create_network((268, 268), 'train_net')
+
+    # create a larger network for faster prediction
+    create_network((322, 322), 'test_net')
